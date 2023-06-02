e8bcf39ca731779e56755f41bcfa2890f01232b2
Volodymyr Romanov <134746871+Romanovw@users.noreply.github.com>
2023-06-03 00:01:47 +0200

    Update for-drivers-mackiewicza.js

--- JS/for-drivers-mackiewicza.js
+++ JS/for-drivers-mackiewicza.js
@@ -587,8 +587,7 @@ const per12 = document.getElementById('per12');
 function countOfShifts (){
 function persons (per, name){
     per.textContent = name.value
-}
-         
+}        
 persons (per1, name1);
 persons (per2, name2);
 persons (per3, name3);
@@ -600,24 +599,23 @@ persons (per8, name8);
 persons (per9, name9);
 persons (per10, name10);
 persons (per11, name11);
-persons (per12, name12);
-                                     
-         
-function countShifts (name, countSh){
+persons (per12, name12);                                    
+function countShifts(name, countSh) {
     let count = 0;
-    for (let i = 0, row; row = schedule.rows[i]; i++) {
-    for (let j = 0, cell; cell = row.cells[j]; j++) {
-        if (cell.textContent == name.value) {
-            count++;
+    const searchValue = name.value.toLowerCase();
+    for (let i = 0, row; (row = schedule.rows[i]); i++) {
+      for (let j = 0, cell; (cell = row.cells[j]); j++) {
+        const cellValue = cell.textContent.toLowerCase();
+        if (cellValue.includes(searchValue)) {
+          count++;
         }
-    }  
+      }
     }
     countSh.textContent = count;
-    if (name.value === '') {
-    countSh.textContent = '';
+    if (name.value === "") {
+      countSh.textContent = "";
     }
-    };
-                         
+  }                       
 countShifts (name1, countSh1);
 countShifts (name2, countSh2);
 countShifts (name3, countSh3);
@@ -631,6 +629,12 @@ countShifts (name10, countSh10);
 countShifts (name11, countSh11);
 countShifts (name12, countSh12);
 };  
+const tableCells = scheduleTable.querySelectorAll('td');
+tableCells.forEach(function(cell) {
+  cell.addEventListener('input', function() {
+    countOfShifts();
+  });
+});  
 
 sellsMenuForDay (mon11, mon12, allForMon1);
 sellsMenuForDay (mon21, mon22, allForMon2);
@@ -663,9 +667,42 @@ const wholeDayP12 = document.getElementById ('wholeDayP12');
 // Button that launches all generation fields
 let generate = document.getElementById('generate');
 generate.addEventListener('click', function() {
+    deleteAll (mon11);
+    deleteAll (mon12);
+    deleteAll (mon21);
+    deleteAll (mon22);
 
-         //a function that excludes the same name on the second shift of the previous day and the first of the next 
-         function notRepeat (previousDay21, nextDay11,
+    deleteAll (tue11);
+    deleteAll (tue12);
+    deleteAll (tue21);
+    deleteAll (tue22);
+
+    deleteAll (wed11);
+    deleteAll (wed12);
+    deleteAll (wed21);
+    deleteAll (wed22);
+
+    deleteAll (thu11);
+    deleteAll (thu12);
+    deleteAll (thu21);
+    deleteAll (thu22);
+
+    deleteAll (fri11);
+    deleteAll (fri12);
+    deleteAll (fri21);
+    deleteAll (fri22);
+
+    deleteAll (sat11);
+    deleteAll (sat12);
+    deleteAll (sat21);
+    deleteAll (sat22);
+
+    deleteAll (sun11);
+    deleteAll (sun12);
+    deleteAll (sun21);
+    deleteAll (sun22);
+//a function that excludes the same name on the second shift of the previous day and the first of the next 
+function notRepeat (previousDay21, nextDay11,
             randomIndexNextDay11, allForNextDay1, 
                     randomElementNextDay11, randomIndexPreviousDay21, allForPreviousDay2, 
                     randomElementPreviousDay21) 
@@ -709,7 +746,7 @@ generate.addEventListener('click', function() {
                         }
                         }
                         }
-         };
+     };
 
     generateForAllDays (randomIndexMon11, allForMon1, 
                             randomElementMon11, 
